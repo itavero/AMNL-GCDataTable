3496d7b613570ab1b7ca25ad02a74a2291983dc4
Arno Moonen <info@arnom.nl>
2013-06-05 12:44:29 +0200

    Added basic Travis CI config

---
+++ test/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of AMNL-GCDataTable.
+ * 
+ * Copyright (c) 2013, Arno Moonen <info@arnom.nl>
+ * 
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ * 
+ * @author Arno Moonen <info@arnom.nl>
+ * @copyright Copyright (c) 2013, Arno Moonen <info@arnom.nl>
+ */
+#namespace AMNL-GCDataTable;
+
+/**
+ * bootstrap class
+ *
+ * @author Arno Moonen <info@arnom.nl>
+ */
+class bootstrap
+{
+    // TODO Implement bootstrap class
+}
